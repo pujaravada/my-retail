1ec799d7e3aa302016f1b8a75071224d9a820464
pujaravada <puja.ravada@gmail.com>
2022-08-29 20:59:13 -0500

    externalize the client url

--- src/main/java/com/pravada/retailapp/myRetail/redskyClient/RedskyClient.java
+++ src/main/java/com/pravada/retailapp/myRetail/redskyClient/RedskyClient.java
@@ -16,28 +16,22 @@ import com.pravada.retailapp.myRetail.dto.RedskyClientProductResponseDTO;
 @Component
 public class RedskyClient {
 
-//	@Autowired
-//	private RestTemplate restTemplate;
+	@Autowired
+	private Environment env;
 
-	// TODO - Add retry
-	//tracing
-	//Add post call
 	public RedskyClientProductResponseDTO getProductInfo(int productId) {
 		RedskyClientProductResponseDTO response = null;
 		RestTemplate restTemplate = new RestTemplate();
 		final String baseUrl = "https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=13860428";
 		URI uri;
 		try {
-			uri = new URI(baseUrl);
+			uri = new URI(env.getProperty("redsky.client.url"));
 			ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
 			System.out.println("RESPONSE IS ... " + result);
-			
+
 			//convert response to json object
-			
-			
 			Gson g = new Gson();  
 			response = g.fromJson(result.getBody(), RedskyClientProductResponseDTO.class) ; 
-
 		} catch (final HttpStatusCodeException e) {
 			throw new RestClientException(e.getRawStatusCode() + " " + e.getResponseBodyAsString());
 		} catch (URISyntaxException e1) {
